1ed1c0eec41c6d2319ee570c8799ecc65e85d164
Josh Dolitsky <jdolitsky@gmail.com>
2014-06-17 22:34:27 -0700

    adding only/unless support

--- dockerspaniel/__init__.py
+++ dockerspaniel/__init__.py
@@ -1,15 +1,60 @@
 from __future__ import print_function
+import copy
 import json
+import os
 
 
 def generateContents(spaniel, tags=[], is_child=False, root_dir=None):
     contents = []
-    contents.append('FROM '+spaniel['from'])
 
-    if spaniel.get('maintainer') is not None:
+    if not is_child and not spaniel['from']:
+        raise Exception('\'from\' attribute does not exist.')
+
+    if spaniel['from']:
+        contents.append('FROM '+spaniel['from'])
+
+    if isinstance(tags, basestring):
+        tags = [tags] 
+
+    if spaniel.get('maintainer'):
         contents.append('MAINTAINER '+spaniel['maintainer'])
 
+    data = {}
+    if spaniel.get('defaults'):
+        data = copy.deepcopy(spaniel['defaults'])
+    
+    for key in os.environ.keys():
+        if len(key) > 3 and key[:3] == 'DS_':
+            data[key[3:].lower()] = os.environ[key]
+
+    if not spaniel.get('steps'):
+        return "\n".join(contents)
+
     for step in spaniel['steps']:
+        if step.get('unless'):
+            skip = False
+            for tag in step['unless']:
+                if tag in tags:
+                    skip = True
+                    break
+            if skip:
+                continue
+
+        if step.get('only'):
+            skip = False
+            for tag in step['only']:
+                if tag not in tags:
+                    skip = True
+                    break
+            if skip:
+                continue
+
+        if step.get('comment') or step.get('newline'):
+            contents.append('')
+        
+        if step.get('comment'):
+            contents.append('# '+step['comment'])
+
         tmp = step['instruction'].upper()+' '
         tmp += step['arguments']
         contents.append(tmp)
