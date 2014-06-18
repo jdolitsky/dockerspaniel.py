11f012fa5d5d3cfc27cc9dcec6d21eac2e70fad7
Josh Dolitsky <jdolitsky@gmail.com>
2014-06-18 01:27:36 -0700

    adding requirements.txt to MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python
 
-import os
 from distutils.core import setup
 
-r_txt = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
-
-with open(r_txt) as f:
+with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(name='dockerspaniel',
-      version='0.1.2',
+      version='0.1.3',
       description='Create Dockerfiles from JSON',
       author='Josh Dolitsky',
       author_email='jdolitsky@gmail.com',
       url='https://github.com/jdolitsky/dockerspaniel.py',
-      download_url='https://github.com/jdolitsky/dockerspaniel.py/tarball/0.1.2',
+      download_url='https://github.com/jdolitsky/dockerspaniel.py/tarball/0.1.3',
       packages=['dockerspaniel'],
       keywords=['docker','dockerfile','json','dockerspaniel','spaniel','spanielfile'],
       install_requires=required,
