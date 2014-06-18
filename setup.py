d5d3fefa1c3abc1a255a48a779aa0066e19cfb0f
Josh Dolitsky <jdolitsky@gmail.com>
2014-06-18 01:20:41 -0700

    fixing setup.py (again)

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ with open(r_txt) as f:
     required = f.read().splitlines()
 
 setup(name='dockerspaniel',
-      version='0.1.1',
+      version='0.1.2',
       description='Create Dockerfiles from JSON',
       author='Josh Dolitsky',
       author_email='jdolitsky@gmail.com',
       url='https://github.com/jdolitsky/dockerspaniel.py',
-      download_url='https://github.com/jdolitsky/dockerspaniel.py/tarball/0.1.0',
+      download_url='https://github.com/jdolitsky/dockerspaniel.py/tarball/0.1.2',
       packages=['dockerspaniel'],
       keywords=['docker','dockerfile','json','dockerspaniel','spaniel','spanielfile'],
       install_requires=required,
