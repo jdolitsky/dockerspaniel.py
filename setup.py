5c4d0c8d5896bcb57b1df13499b0e66c1db812e4
Josh Dolitsky <jdolitsky@gmail.com>
2014-06-18 00:33:23 -0700

    pypi files

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(name='dockerspaniel',
+      version='0.1.0',
+      description='Create Dockerfiles from JSON',
+      author='Josh Dolitsky',
+      author_email='jdolitsky@gmail.com',
+      url='https://github.com/jdolitsky/dockerspaniel.py',
+      download_url='https://github.com/jdolitsky/dockerspaniel.py/tarball/0.1.0',
+      packages=['dockerspaniel'],
+      keywords=['docker','dockerfile','json','dockerspaniel','spaniel','spanielfile'],
+      install_requires=required,
+      classifiers=[],
+     )
